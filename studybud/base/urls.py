a98aaa180f6457fb43afadf7e00940b2f2210c20
Eric Fackelman <12144776+hackinmonster@users.noreply.github.com>
2024-09-14 18:38:27 -0400

    3:53:25- Static Files

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     #passing in the id into the url, comes from the parameter in the room function in views.py
     #this allows for dynamic routing
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>', views.userProfile, name="user-profile"),
 
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
