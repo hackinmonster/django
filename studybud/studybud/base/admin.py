c943d43085f86e505d4bfd997d5e1f14940a44f8
Eric Fackelman <12144776+hackinmonster@users.noreply.github.com>
2024-09-14 17:34:12 -0400

    3:27:30 - Activity Feed

--- studybud/studybud/base/admin.py
+++ studybud/studybud/base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Room, Topic, Message
+
+
+    #we want to be able too view this item and work with it in the admin panel
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
--- studybud/studybud/base/urls.py
+++ studybud/studybud/base/urls.py
@@ -2,7 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+
+
     path('', views.home, name='home'),
-    path('room/', views.room, name='room'),
-    
+    #passing in the id into the url, comes from the parameter in the room function in views.py
+    #this allows for dynamic routing
+    path('room/<str:pk>/', views.room, name='room'),
+
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
+
 ]
