df9582846b47e42c7fec48556c4f71071f63f1ab
Eric Fackelman <12144776+hackinmonster@users.noreply.github.com>
2024-09-24 22:46:01 -0400

    5:53:00 - Django REST Framework

--- studybud/studybud/base/urls.py
+++ studybud/studybud/base/urls.py
@@ -13,10 +13,13 @@ urlpatterns = [
     #passing in the id into the url, comes from the parameter in the room function in views.py
     #this allows for dynamic routing
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>', views.userProfile, name="user-profile"),
 
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
     path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
-
+    path('update-user/', views.updateUser, name='update-user'),
+    path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 ]
